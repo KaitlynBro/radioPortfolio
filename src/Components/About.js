d758dd906e5029c9a6d5ac520cf854d4a0197031
KaitlynBro <kaitlynbrouwers@gmail.com>
2019-05-17 20:36:31 -0400

    about section done...dont like the design but will come back to this

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,28 +8,39 @@ class About extends React.Component {
             <div className="about-wrapper">
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates eaque id in excepturi ad quo sed eos cum, quas magnam doloribus nesciunt corporis enim neque! Quos non provident repellendus dolores cupiditate.</p>
                 <div className="about-skills my-5">
-                    <div className="skill">
-                        <div className="skill-image">
+                    <div className="skill col-3">
+                        <div className="skill-image one">
                             <img src={RadioIcon} alt="" />
                             {/* Icons made by <a href="https://www.flaticon.com/authors/vectors-market" title="Vectors Market">Vectors Market</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a> */}
                         </div>
                         <h3>Radio Broadcaster</h3>
+                        <p>
+                            venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin
+                        </p>
                     </div>
-                    <div className="skill">
-                        <div className="skill-image">
+                    <div className="skill col-3">
+                        <div className="skill-image two">
                             <img src={MicrophoneIcon} alt="" />
                             {/* Icons made by <a href="https://www.flaticon.com/authors/vectors-market" title="Vectors Market">Vectors Market</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a> */}
                         </div>
                         <h3>Journalist</h3>
+                        <p>
+                            venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin
+                        </p>
                     </div>
-                    <div className="skill">
-                        <div className="skill-image container">
+                    <div className="skill col-3">
+                        <div className="skill-image three">
+                            <div className="container">
                             <span></span>
                             <span></span>
                             <span></span>
                             <span></span>
+                            </div>
                         </div>
                         <h3>Podcast Host</h3>
+                        <p>
+                            venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin
+                        </p>
                     </div>
                 </div>
             </div>
